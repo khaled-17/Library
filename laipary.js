4f231a7e9f83fe272058c23af4c86d69943d114d
khaled-17 <41763585+khaled-17@users.noreply.github.com>
2022-09-15 16:43:59 +0200

    from dokan

--- laipary.js
+++ laipary.js
@@ -1,90 +1,28 @@
-// document.cookie="name=khale";
-
-
-function ITICREATOR() {
-    console.log("From ITI STUSEND, the BY 💚💛 Mohamed Awaad and 🆗 Khaled Ram, thank you Awaad and thank ITI ");
-console.log("===========");
-console.log("===========");
-console.log("TO SEE👁‍🗨 ALL function  ItiAllMetiod() ");
-}
-function version() {
-    console.log("version number: 00,1  ");
-    console.log("version nickname: a.salah  ");
-
+function setCookie(cookieName, cookieValue, exdays) {
+  const d = new Date();
+  d.setDate(d.getDate() + exdays);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires;
 }
 
-function ItiAllMetiod() {
-    console.log("ITICREATOR()");
-    console.log("CREATOR()");
-    console.log("ItiAllCookiy()");
-    console.log("ItiDeletAllCookiy()");
-    console.log("ItiGetcookey()");
-    console.log("AND ItiGetcookey/ItiDelet/ItiSerch()");
-
-}
-ITICREATOR()
-function ItiSetcookie(key, value, exdate) {
-    var date = new Date(exdate);
-    document.cookie = key + "=" + value + ";expires=" + date + ";";
-}
-// setcookie("color","fdf","2022-10-20")
-function ItiAllCookiy() {
-
-    var cookie = document.cookie.split(";");
-    var cookieArray = [];
-
-    for (const i of cookie) {
-        var x = i.split("=");
-        cookieArray[x[0].trim()] = x[1];
+function getCookie(cookieName) {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    c = c.trimStart();
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
     }
-
-    return cookieArray
+  }
+  return "";
 }
 
-function ItiDeletAllCookiy() {
-
-
-    for (const i of cookie) {
-        var x = i.split("=");
-        allco = cookieArray[x[0].trim()] = x[0]
-        console.log(document.cookie = allco + "=;expires=")
-    }
-
-    return cookieArray
+function hasCookie(cookieName) {
+  return getCookie(cookieName) !== "";
 }
 
-///////////
-
-// for (const key in allCookiy()) {
-//     console.log(key);
-//     console.log(allCookiy()[key]);
-
-
-// }
-
-function ItiSerch(item) {
-    return allCookiy().hasOwnProperty(item)
-
+function deleteCookie(cookieName) {
+  document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970";
 }
-
-function ItiDelet(delitem) {
-
-    if (serch(delitem)) {
-        document.cookie = delitem + "=;expires=;";
-
-    } else {
-        throw (" its not fond")
-
-    }
-
-}
-
-function ItiGetcookey(key) {
-    if (serch(key)) {
-        return allCookiy()[key]
-
-    } else {
-        throw (" not  fond key")
-    }
-}
-
